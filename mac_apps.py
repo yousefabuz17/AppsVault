9bb075974f8cc60ed205209f5a21d1f7c9a1ea6d
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-23 00:51:25 -0400

    changes

--- mac_apps.py
+++ mac_apps.py
@@ -1,9 +1,11 @@
 import os
 import plistlib
 from pathlib import Path
+import math
 from collections import OrderedDict
-
-
+import re
+import subprocess
+from pprint import pprint
 
 class Apps:
     def __init__(self):
@@ -12,32 +14,36 @@ class Apps:
     
     def get_file_plists(self):
         plist_path = '/Applications/{}/Contents/Info.plist'
-        files_ = filter(lambda i: i.endswith('.app'), sorted(os.listdir('/Applications'), key=lambda i: i.lower()))
-        files = list(plist_path.format(i) for i in files_)
-        return files
+        app_path = '/Applications'
+        files_and_dirs = filter(lambda i: not i.startswith('.'), os.listdir(app_path))
+        paths_fixed = [os.path.join(app_path, i) for i in files_and_dirs]
+        files_, dirs_ = [i for i in paths_fixed if i.endswith('.app')], [i for i in paths_fixed if not i.endswith('.app') and not i==f'{app_path}/Utilities']
+        all_files = [plist_path.format(i.split('/')[-1]) for i in files_] + [plist_path.format(i.split('/')[-1]) for i in dirs_]
+        all_files = sorted(all_files, key=lambda i: i.split('/')[2].lower())
+        return all_files
     
-    def convert_size(self, file):
+    def convert_size(self, file_size):
         size_units = ['B', 'KB', 'MB', 'GB', 'TB']
-        factor = 1024.0
-        for unit in size_units:
-            if file < factor:
-                return f"{file:.2f} {unit}"
-            file /= factor
+        factor = 1024
         
-        return f"{file:.2f} {size_units[-1]}"
+        
+        
     
     def get_app_info(self):
         app_info = OrderedDict()
         file_plists = self.get_file_plists()
-        for idx, file_path in enumerate(file_plists, start=1):
-            file = open(file_path, 'rb').read()
-            info_plist = plistlib.loads(file)
-            app_info[f'Application {idx}'] = {
-                                    'Name': info_plist.get('CFBundleName', 'N/A'),
-                                    'Version': info_plist.get('CFBundleShortVersionString', 'N/A'),
-                                    'Minimum System Version': info_plist.get('LSMinimumSystemVersion', 'N/A'),
-                                    'Size': self.convert_size(os.path.getsize(file_path))
-                                    }
+        for _, file_path in enumerate(file_plists, start=1):
+            try:
+                file_name = file_path.split('/')[2]
+                file = open(file_path, 'rb').read()
+                info_plist = plistlib.loads(file)
+                app_info[info_plist.get('CFBundleName', file_name)] = {
+                                        'Version': info_plist.get('CFBundleShortVersionString', 'N/A'),
+                                        'Minimum System Version': info_plist.get('LSMinimumSystemVersion', 'N/A'),
+                                        'Size': os.path.getsize(file_path)
+                                        }
+            except FileNotFoundError:
+                pass
         
         return app_info
         
@@ -47,7 +53,7 @@ class Apps:
 
 def main():
     apps = Apps()
-    print(apps.get_app_info())
+    pprint(apps.get_app_info())
 
 if __name__ == '__main__':
     main()
